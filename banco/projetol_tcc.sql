aa50e2c30edfb7ebbd71f00e57b15561a68f9d55
lrohde <luan.rohde@ceepcascavel.com.br>
2017-11-29 00:22:55 -0200

    Banco

--- banco/projetol_tcc.sql
+++ banco/projetol_tcc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 26-Nov-2017 às 17:49
+-- Generation Time: 29-Nov-2017 às 03:21
 -- Versão do servidor: 10.1.22-MariaDB
 -- PHP Version: 7.1.4
 
@@ -48,40 +48,28 @@ INSERT INTO `aluno` (`cgm`, `curso`, `turma`, `id`, `idpessoa`) VALUES
 (7657567, 'Informatica', '4A', 20, 38),
 (556456465, 'Informatica', '4A', 21, 39),
 (2147483647, 'Informatica', '4A', 22, 40),
-(2147483647, 'Informatica', '4A', 23, 45),
-(6767676, 'Informatica', '3a', 24, 46),
-(4234234, 'Informatica', '4A', 28, 60),
-(2147483647, 'Informatica', '4A', 29, 61),
-(448807959, 'Informatica', '4a', 34, 68);
+(2147483647, 'Informatica', '4A', 23, 45);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `alunozz`
+-- Estrutura da tabela `ceep`
 --
 
-CREATE TABLE `alunozz` (
-  `id_aluno` int(100) NOT NULL,
+CREATE TABLE `ceep` (
+  `id` int(11) NOT NULL,
   `nome` varchar(100) NOT NULL,
-  `nascimento` varchar(10) NOT NULL,
-  `rg` varchar(12) NOT NULL,
-  `cgm` varchar(30) NOT NULL,
-  `curso` varchar(30) NOT NULL,
-  `turma` varchar(30) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `senha` int(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+  `nascimento` varchar(15) NOT NULL,
+  `rg` varchar(15) NOT NULL,
+  `cgm` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Extraindo dados da tabela `alunozz`
+-- Extraindo dados da tabela `ceep`
 --
 
-INSERT INTO `alunozz` (`id_aluno`, `nome`, `nascimento`, `rg`, `cgm`, `curso`, `turma`, `email`, `senha`) VALUES
-(1, 'Vinícius', '24/06/1999', '12.584.317-4', '889409126', 'Informatica', '4 ano', 'vinistanoga@gmail.com', 123),
-(2, 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 123),
-(3, 'Maria', '18/06/1999', '12121212', '121212', 'Informatica', '4 ano', 'maria@hotmail.com', 123),
-(4, 'Andre', '20/04/1980', '131954921', '448807959', 'Eletronica', '4a', 'jandrey@jandrey', 123),
-(6, 'Luan Teste', '05/03/1999', '131954921', '448807959', 'Informatica', '4A', 'luanrohde11@gmail.com', 0);
+INSERT INTO `ceep` (`id`, `nome`, `nascimento`, `rg`, `cgm`) VALUES
+(1, 'MARIA EDUARDA PACENTCHUK RODRIGUES', '18/06/1999', '97.216.231-1', 889408367);
 
 -- --------------------------------------------------------
 
@@ -91,14 +79,14 @@ INSERT INTO `alunozz` (`id_aluno`, `nome`, `nascimento`, `rg`, `cgm`, `curso`, `
 
 CREATE TABLE `curso` (
   `id_curso` int(11) NOT NULL,
-  `nome` varchar(50) NOT NULL
+  `nome_curso` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `curso`
 --
 
-INSERT INTO `curso` (`id_curso`, `nome`) VALUES
+INSERT INTO `curso` (`id_curso`, `nome_curso`) VALUES
 (1, 'Informatica'),
 (2, 'Administracao'),
 (3, 'Eletronica'),
@@ -114,7 +102,6 @@ INSERT INTO `curso` (`id_curso`, `nome`) VALUES
 CREATE TABLE `disciplina` (
   `id_disciplina` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
-  `curso` varchar(50) NOT NULL,
   `id_curso` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -122,8 +109,9 @@ CREATE TABLE `disciplina` (
 -- Extraindo dados da tabela `disciplina`
 --
 
-INSERT INTO `disciplina` (`id_disciplina`, `nome`, `curso`, `id_curso`) VALUES
-(10, 'historia', '', 1);
+INSERT INTO `disciplina` (`id_disciplina`, `nome`, `id_curso`) VALUES
+(10, 'historia', 1),
+(11, 'WEB', 1);
 
 -- --------------------------------------------------------
 
@@ -148,50 +136,6 @@ INSERT INTO `funcionario` (`id`, `cargo`, `cpf`, `idpessoa`) VALUES
 (5, '3', '465.665.656-5', 43),
 (6, '3', '645.456.456-4', 44),
 (14, '3', '545.454.545-4', 54);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `funcionariozz`
---
-
-CREATE TABLE `funcionariozz` (
-  `id_funcionario` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `nascimento` varchar(10) NOT NULL,
-  `rg` varchar(12) NOT NULL,
-  `cpf` varchar(15) NOT NULL,
-  `cargo` varchar(20) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `senha` int(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
-
---
--- Extraindo dados da tabela `funcionariozz`
---
-
-INSERT INTO `funcionariozz` (`id_funcionario`, `nome`, `nascimento`, `rg`, `cpf`, `cargo`, `email`, `senha`) VALUES
-(1, 'Vinicius', '24/06/1999', '12.584.317-4', '069.368.299-02', 'Prof', 'vinicius@email.com', 123),
-(2, 'Vinicius', '24/06/1999', '12.584.317-4', '069.368.299-02', 'Prof', 'vinicius@email.com', 123),
-(3, 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 123),
-(4, 'Vinicius', '123', '123', '123', 'professor', 'vini.stanoga', 123),
-(5, 'Luan', '1234', '1234', '1234', 'professor', 'luan.rohde', 123);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `info_aluno`
---
-
-CREATE TABLE `info_aluno` (
-  `id_info_aluno` int(11) NOT NULL,
-  `nome_aluno` varchar(100) NOT NULL,
-  `nome_professor` varchar(100) NOT NULL,
-  `notas` float NOT NULL,
-  `reclamacoes` varchar(255) NOT NULL,
-  `observacoes` varchar(255) NOT NULL,
-  `elogios` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -273,11 +217,8 @@ INSERT INTO `pessoa` (`id`, `nome`, `rg`, `cpf`, `nascimento`, `foto`, `email`, 
 (43, 'Celma', '54.545.454-5', '', '65/56/5656', 'WhatsApp Image 2017-10-09 at 7.47.33 PM.jpeg', 'celma.burille@ceepcascavel.com.br', '123', 'ativo', 3, 'celma.burille', '2017-10-19 06:54:26', '2017-10-19 20:54:50'),
 (44, 'Vander', '65.656.767-6', '', '76/76/7676', 'WhatsApp Image 2017-10-09 at 7.47.33 PM.jpeg', 'vander.ceep@ceepcascavel.com.br', 'vander123', 'ativo', 3, 'vander.ceep', '2017-10-19 06:58:36', '2017-10-19 20:58:51'),
 (45, 'AurÃ©lio', '76.767.676-7', '', '56/75/6756', 'teste.jpeg', 'vini.aurelio@ceepcascavel.com.br', '12345', 'ativo', 2, 'vini.aurelio', '2017-10-19 07:20:03', '2017-10-19 21:20:53'),
-(46, 'Florisbelo', '56.767.767-6', '', '45/65/4665', 'teste.jpeg', 'floris.belo@ceepcascavel.com.br', '123', 'ativo', 2, 'floris.belo', '2017-10-20 08:35:56', '2017-10-20 22:38:10'),
 (54, 'Jandrey', '34.234.342-1', '', '45/34/5454', NULL, 'andre.jandrey@ceepcascavel.com.br', '1234', 'ativo', 3, 'andre.jandrey', '2017-10-29 11:51:27', '2017-11-26 15:53:18'),
-(60, 'teste', '45.446.466-3', '', '54/63/4636', NULL, 'teste.teste@ceepcascavel.com.br', '123', 'ativo', 2, 'teste.teste', '2017-10-31 10:44:01', '2017-11-07 17:01:21'),
-(61, 'Luan', '13.195.492-1', '', '05/03/1999', NULL, 'luan.sgu@ceepcascavel.com.br', '123', 'ativo', 2, 'luan.sgu', '2017-11-02 04:21:28', '2017-11-07 17:01:21'),
-(68, 'Luan Rohde', '13.195.492-1', '088.035.059-89', '05/03/1999', 'perfil.jpg', 'luan.rohde@ceepcascavel.com.br', '123', 'ativo', 2, 'luan.rohde', '2017-11-07 03:21:07', '2017-11-09 10:30:01');
+(76, 'Antonio', '54.646.456-4', '456.565.656-56', '46/56/5665', NULL, 'antonio@ceepcascavel.com.br', '123', 'pendente', 2, 'antonio', '2017-11-29 12:18:40', '2017-11-29 02:18:40');
 
 -- --------------------------------------------------------
 
@@ -287,12 +228,19 @@ INSERT INTO `pessoa` (`id`, `nome`, `rg`, `cpf`, `nascimento`, `foto`, `email`, 
 
 CREATE TABLE `turma` (
   `id_turma` int(11) NOT NULL,
-  `ano` int(10) NOT NULL,
-  `serie` varchar(10) NOT NULL,
+  `nome` varchar(10) NOT NULL,
   `periodo` varchar(50) NOT NULL,
   `id_curso` int(100) NOT NULL,
-  `data_inicio` varchar(10) NOT NULL
+  `data_inicio` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `turma`
+--
+
+INSERT INTO `turma` (`id_turma`, `nome`, `periodo`, `id_curso`, `data_inicio`) VALUES
+(1, '4A', 'matutino', 1, '2017-11-28'),
+(2, '3A', 'matutino', 1, '2017-11-28');
 
 --
 -- Indexes for dumped tables
@@ -306,10 +254,10 @@ ALTER TABLE `aluno`
   ADD KEY `idpessoa` (`idpessoa`);
 
 --
--- Indexes for table `alunozz`
+-- Indexes for table `ceep`
 --
-ALTER TABLE `alunozz`
-  ADD PRIMARY KEY (`id_aluno`);
+ALTER TABLE `ceep`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `curso`
@@ -330,18 +278,6 @@ ALTER TABLE `disciplina`
 ALTER TABLE `funcionario`
   ADD PRIMARY KEY (`id`),
   ADD KEY `idpessoa` (`idpessoa`);
-
---
--- Indexes for table `funcionariozz`
---
-ALTER TABLE `funcionariozz`
-  ADD PRIMARY KEY (`id_funcionario`);
-
---
--- Indexes for table `info_aluno`
---
-ALTER TABLE `info_aluno`
-  ADD PRIMARY KEY (`id_info_aluno`);
 
 --
 -- Indexes for table `login`
@@ -379,12 +315,12 @@ ALTER TABLE `turma`
 -- AUTO_INCREMENT for table `aluno`
 --
 ALTER TABLE `aluno`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 --
--- AUTO_INCREMENT for table `alunozz`
+-- AUTO_INCREMENT for table `ceep`
 --
-ALTER TABLE `alunozz`
-  MODIFY `id_aluno` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+ALTER TABLE `ceep`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `curso`
 --
@@ -394,22 +330,12 @@ ALTER TABLE `curso`
 -- AUTO_INCREMENT for table `disciplina`
 --
 ALTER TABLE `disciplina`
-  MODIFY `id_disciplina` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_disciplina` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `funcionario`
 --
 ALTER TABLE `funcionario`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
---
--- AUTO_INCREMENT for table `funcionariozz`
---
-ALTER TABLE `funcionariozz`
-  MODIFY `id_funcionario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT for table `info_aluno`
---
-ALTER TABLE `info_aluno`
-  MODIFY `id_info_aluno` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `login`
 --
@@ -424,12 +350,12 @@ ALTER TABLE `ocorrencia`
 -- AUTO_INCREMENT for table `pessoa`
 --
 ALTER TABLE `pessoa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 --
 -- AUTO_INCREMENT for table `turma`
 --
 ALTER TABLE `turma`
-  MODIFY `id_turma` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_turma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Constraints for dumped tables
 --
